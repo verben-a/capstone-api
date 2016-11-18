caa7258c4745e04ca97982127baaad5f222403ef
verben-a <alina.verbenchuk@gmail.com>
2016-11-18 23:12:47 +0000

    Search+dates+ranked prices fixed

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // $(function(){
-	// are the same
+// are the same
 //$(document).ready(function() {
 
 $(document).ready(function() {
 
-	/// write date range event here (it is a little separate)
+    /// write date range event here (it is a little separate)
 
     // $('input[name="flightdate"]').daterangepicker();
 
-	// form code below is for when you actually submit the form (not selecting a date)
-	
+    // form code below is for when you actually submit the form (not selecting a date)
+
     $('form').submit(function(event) {
         event.preventDefault();
 
         var request = {
             origin_iata: $('#search').val(),
-            period: $('input[name="flightdate"]').val()+":season",
+            period: $('input[name="flightdate"]').val() + ":season",
             direct: true,
             one_way: true,
             no_visa: true,
@@ -25,31 +25,38 @@ $(document).ready(function() {
         };
 
 
-// origin_iata – IATA code for the point of departure. IATA code is shown by uppercase letters, for example MOW.
-// period – Period of dates to search for.
-// direct – Whether non-stop flights are available.
-// one_way – “true” for one-way tickets, “false” for round trips.
-// no_visa – Payment not made with a Visa card.
-// schengen – Can the ticket be changed.
-// need_visa – Visa required (or not).
-// locale – Search language.
-// min_trip_duration_in_days – Shortest trip duration (in days).
-// max_trip_duration_in_days – Longest trip duration (in days).
-
         $.getJSON("http://map.aviasales.ru/prices.json", request).done(function(results) {
             // console.log(results);
+            console.log('here')
+            var length = results.length;
+            // $(".results").append("<thead><tr><th>Destination</th><th>Price (USD)</th></tr></thead>");
             $.each(results, function(index, item) {
                 console.log(item);
-
-                $(".results").append(item.destination + " " + item.value + "<br/>");
+                var price = function(data) {
+                    console.log('data', data);
+                    fx.rates = data.rates
+                    // var rate = fx(1).from("EUR").to("USD")
+                    var rate = fx.convert(item.value, {from: "RUB", to: "USD"});
+                    console.log("£1 = " + rate.toFixed(4))
+                    $(".results-body").append("<tr><td>" + item.destination + "</td><td>" + rate.toFixed(2) + "</td></tr>");
+                    /// if the last Result has been reahed, show the data
+                    if (length-1 == index){
+                        // show the table
+                        $('.results').DataTable();
+                    }
+                }
+                $.getJSON("http://api.fixer.io/latest", price);
             });
+            
+            
         }).fail(function(data) {
-
+            console.log('failed', data);
         });
 
 
     });
 });
+
 
 
 // $('input[name="daterange"]').val().split('-')[0].replace(/\s/g, '').replace('/', '-').replace('/', '-')
